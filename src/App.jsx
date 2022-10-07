e4fb8e14a4a2aab28171b6d4bf850a1902409a93
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-10-07 19:38:58 +0600

    added react-slick for slider and applied in intro section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import Promo from './sections/Promo';
 import Service from './sections/Service';
 import Team from './sections/Team';
 import Testimonial from './sections/Testimonial';
+// Import css files for slider
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
 
 const App = () => {
   return (
--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -1,65 +1,84 @@
 import React from "react";
+import Slider from "react-slick";
 
 const Main = () => {
+  const settings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 2000,
+  };
+
+
+
   return (
     <>
-      <section id="banner-part" name='main'>
+      <section id="banner-part" name="main">
         <div className="container">
           <div className="row banner-slider">
-            <div className="col-lg-10 m-auto">
-              <div className="banner-text text-center">
-                <h2>
-                  We Are <span className="typed"></span>
-                </h2>
-                <p>
-                  This is Photoshop's version of Lorem Ipsum. Proin gravida nibh
-                  vel velit auctor aliquet. Aenean sollicitudin, lorem quis
-                  bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                  nibh id elit. Duis sed odio sit amet nibh vulputate cursus a
-                  sit amet mauris. Morbi accumsan ipsum velit.
-                </p>
-                <a href="#">LEARN MORE</a>
+            <Slider {...settings}>
+              <div className="col-lg-10 m-auto">
+                <div className="banner-text text-center">
+                  <h2>
+                    We Are <span className="typed"></span>
+                  </h2>
+                  <p>
+                    This is Photoshop's version of Lorem Ipsum. Proin gravida
+                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                    quis bibendum auctor, nisi elit consequat ipsum, nec
+                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
+                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                    velit.
+                  </p>
+                  <a href="#">LEARN MORE</a>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-10 m-auto">
-              <div className="banner-text text-center">
-                <h2>Complete Front End development</h2>
-                <p>
-                  This is Photoshop's version of Lorem Ipsum. Proin gravida nibh
-                  vel velit auctor aliquet. Aenean sollicitudin, lorem quis
-                  bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                  nibh id elit. Duis sed odio sit amet nibh vulputate cursus a
-                  sit amet mauris. Morbi accumsan ipsum velit.
-                </p>
-                <a href="#">LEARN MORE</a>
+              <div className="col-lg-10 m-auto">
+                <div className="banner-text text-center">
+                  <h2>Complete Front End development</h2>
+                  <p>
+                    This is Photoshop's version of Lorem Ipsum. Proin gravida
+                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                    quis bibendum auctor, nisi elit consequat ipsum, nec
+                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
+                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                    velit.
+                  </p>
+                  <a href="#">LEARN MORE</a>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-10 m-auto">
-              <div className="banner-text text-center">
-                <h2>Complete back End development</h2>
-                <p>
-                  This is Photoshop's version of Lorem Ipsum. Proin gravida nibh
-                  vel velit auctor aliquet. Aenean sollicitudin, lorem quis
-                  bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                  nibh id elit. Duis sed odio sit amet nibh vulputate cursus a
-                  sit amet mauris. Morbi accumsan ipsum velit.
-                </p>
-                <a href="#">LEARN MORE</a>
+              <div className="col-lg-10 m-auto">
+                <div className="banner-text text-center">
+                  <h2>Complete back End development</h2>
+                  <p>
+                    This is Photoshop's version of Lorem Ipsum. Proin gravida
+                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                    quis bibendum auctor, nisi elit consequat ipsum, nec
+                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
+                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                    velit.
+                  </p>
+                  <a href="#">LEARN MORE</a>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-10 m-auto">
-              <div className="banner-text text-center">
-                <h2>Complete Ghraphics Design</h2>
-                <p>
-                  This is Photoshop's version of Lorem Ipsum. Proin gravida nibh
-                  vel velit auctor aliquet. Aenean sollicitudin, lorem quis
-                  bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                  nibh id elit. Duis sed odio sit amet nibh vulputate cursus a
-                  sit amet mauris. Morbi accumsan ipsum velit.
-                </p>
-                <a href="#">LEARN MORE</a>
+              <div className="col-lg-10 m-auto">
+                <div className="banner-text text-center">
+                  <h2>Complete Ghraphics Design</h2>
+                  <p>
+                    This is Photoshop's version of Lorem Ipsum. Proin gravida
+                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                    quis bibendum auctor, nisi elit consequat ipsum, nec
+                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
+                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                    velit.
+                  </p>
+                  <a href="#">LEARN MORE</a>
+                </div>
               </div>
-            </div>
+            </Slider>
           </div>
         </div>
       </section>
