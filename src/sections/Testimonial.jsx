4fc8bb1a1b34a8bfcddbe00e94d0b89127ffe129
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-10-09 10:58:06 +0600

    added testimonial sectin slider

--- src/sections/Testimonial.jsx
+++ src/sections/Testimonial.jsx
@@ -1,164 +1,210 @@
-import React from "react";
+import React, { useState } from "react";
+import { Fade } from "react-reveal";
 import Slider from "react-slick";
+
+const PrevArrow = (props) => {
+  const { onClick } = props;
+
+  return (
+    <i className={`fas fa-chevron-left team-arrow-left`} onClick={onClick}></i>
+  );
+};
+
+const NextArrow = (props) => {
+  const { onClick } = props;
+
+  return (
+    <i
+      className={`fas fa-chevron-right team-arrow-right`}
+      onClick={onClick}
+    ></i>
+  );
+};
 
 const Testimonial = () => {
   const settings = {
     infinite: true,
+    arrows: false,
+    speed: 500,
+    fade: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    dots: true,
-    arrows: false,
-    autoplay: false,
-    autoplaySpeed: 1500,
-  };
+  }
+
+  const [nav1, setNav1] = useState(null);
+  const [nav2, setNav2] = useState(null);
 
   return (
     <>
-      <section id="team-two" name="testimonial">
-        <div className="container">
-          <div className="row text-slider">
-            <Slider {...settings}>
-              <div className="col-lg-10 m-auto">
-                <div className="team-slider-text text-center">
-                  <i className="fas fa-quote-left"></i>
-                  <p>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida
-                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                    quis bibendum auctor, nisi elit consequat ipsum, nec
-                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
-                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
-                    velit. Nam nec tellus a odio tincidunt auctor a ornare odio.
-                    Sed non mauris vitae erat consequat auctor eu in elit.
-                  </p>
-                  <h4>JANE GALADRIEL</h4>
-                  <h5>CEO TENGKUREP</h5>
+      <Fade duration={1000}>
+        <section id="team-two" name="testimonial">
+          <div className="container">
+            <div className="row text-slider">
+              <Slider
+                asNavFor={nav2}
+                {...settings}
+                ref={(slider1) => setNav1(slider1)}
+              >
+                <div className="col-lg-10 m-auto">
+                  <div className="team-slider-text text-center">
+                    <i className="fas fa-quote-left"></i>
+                    <p>
+                      This is Photoshop's version of Lorem Ipsum. Proin gravida
+                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                      quis bibendum auctor, nisi elit consequat ipsum, nec
+                      sagittis sem nibh id elit. Duis sed odio sit amet nibh
+                      vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                      velit. Nam nec tellus a odio tincidunt auctor a ornare
+                      odio. Sed non mauris vitae erat consequat auctor eu in
+                      elit.
+                    </p>
+                    <h4>JANE GALADRIEL</h4>
+                    <h5>CEO TENGKUREP</h5>
+                  </div>
                 </div>
-              </div>
 
-              <div className="col-lg-10 m-auto">
-                <div className="team-slider-text text-center">
-                  <i className="fas fa-quote-left"></i>
-                  <p>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida
-                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                    quis bibendum auctor, nisi elit consequat ipsum, nec
-                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
-                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
-                    velit. Nam nec tellus a odio tincidunt auctor a ornare odio.
-                    Sed non mauris vitae erat consequat auctor eu in elit.
-                  </p>
-                  <h4>Ezekiel</h4>
-                  <h5>Web Developer</h5>
+                <div className="col-lg-10 m-auto">
+                  <div className="team-slider-text text-center">
+                    <i className="fas fa-quote-left"></i>
+                    <p>
+                      This is Photoshop's version of Lorem Ipsum. Proin gravida
+                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                      quis bibendum auctor, nisi elit consequat ipsum, nec
+                      sagittis sem nibh id elit. Duis sed odio sit amet nibh
+                      vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                      velit. Nam nec tellus a odio tincidunt auctor a ornare
+                      odio. Sed non mauris vitae erat consequat auctor eu in
+                      elit.
+                    </p>
+                    <h4>Ezekiel</h4>
+                    <h5>Web Developer</h5>
+                  </div>
                 </div>
-              </div>
 
-              <div className="col-lg-10 m-auto">
-                <div className="team-slider-text text-center">
-                  <i className="fas fa-quote-left"></i>
-                  <p>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida
-                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                    quis bibendum auctor, nisi elit consequat ipsum, nec
-                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
-                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
-                    velit. Nam nec tellus a odio tincidunt auctor a ornare odio.
-                    Sed non mauris vitae erat consequat auctor eu in elit.
-                  </p>
-                  <h4>Gideon</h4>
-                  <h5>UX/UI Designer</h5>
+                <div className="col-lg-10 m-auto">
+                  <div className="team-slider-text text-center">
+                    <i className="fas fa-quote-left"></i>
+                    <p>
+                      This is Photoshop's version of Lorem Ipsum. Proin gravida
+                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                      quis bibendum auctor, nisi elit consequat ipsum, nec
+                      sagittis sem nibh id elit. Duis sed odio sit amet nibh
+                      vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                      velit. Nam nec tellus a odio tincidunt auctor a ornare
+                      odio. Sed non mauris vitae erat consequat auctor eu in
+                      elit.
+                    </p>
+                    <h4>Gideon</h4>
+                    <h5>UX/UI Designer</h5>
+                  </div>
                 </div>
-              </div>
 
-              <div className="col-lg-10 m-auto">
-                <div className="team-slider-text text-center">
-                  <i className="fas fa-quote-left"></i>
-                  <p>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida
-                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                    quis bibendum auctor, nisi elit consequat ipsum, nec
-                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
-                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
-                    velit. Nam nec tellus a odio tincidunt auctor a ornare odio.
-                    Sed non mauris vitae erat consequat auctor eu in elit.
-                  </p>
-                  <h4>Reuben</h4>
-                  <h5>Javascript Developer</h5>
+                <div className="col-lg-10 m-auto">
+                  <div className="team-slider-text text-center">
+                    <i className="fas fa-quote-left"></i>
+                    <p>
+                      This is Photoshop's version of Lorem Ipsum. Proin gravida
+                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                      quis bibendum auctor, nisi elit consequat ipsum, nec
+                      sagittis sem nibh id elit. Duis sed odio sit amet nibh
+                      vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                      velit. Nam nec tellus a odio tincidunt auctor a ornare
+                      odio. Sed non mauris vitae erat consequat auctor eu in
+                      elit.
+                    </p>
+                    <h4>Reuben</h4>
+                    <h5>Javascript Developer</h5>
+                  </div>
                 </div>
-              </div>
 
-              <div className="col-lg-10 m-auto">
-                <div className="team-slider-text text-center">
-                  <i className="fas fa-quote-left"></i>
-                  <p>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida
-                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                    quis bibendum auctor, nisi elit consequat ipsum, nec
-                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
-                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
-                    velit. Nam nec tellus a odio tincidunt auctor a ornare odio.
-                    Sed non mauris vitae erat consequat auctor eu in elit.
-                  </p>
-                  <h4>Frederick</h4>
-                  <h5>Marketing Officer</h5>
+                <div className="col-lg-10 m-auto">
+                  <div className="team-slider-text text-center">
+                    <i className="fas fa-quote-left"></i>
+                    <p>
+                      This is Photoshop's version of Lorem Ipsum. Proin gravida
+                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                      quis bibendum auctor, nisi elit consequat ipsum, nec
+                      sagittis sem nibh id elit. Duis sed odio sit amet nibh
+                      vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                      velit. Nam nec tellus a odio tincidunt auctor a ornare
+                      odio. Sed non mauris vitae erat consequat auctor eu in
+                      elit.
+                    </p>
+                    <h4>Frederick</h4>
+                    <h5>Marketing Officer</h5>
+                  </div>
                 </div>
-              </div>
 
-              <div className="col-lg-10 m-auto">
-                <div className="team-slider-text text-center">
-                  <i className="fas fa-quote-left"></i>
-                  <p>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida
-                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                    quis bibendum auctor, nisi elit consequat ipsum, nec
-                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
-                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
-                    velit. Nam nec tellus a odio tincidunt auctor a ornare odio.
-                    Sed non mauris vitae erat consequat auctor eu in elit.
-                  </p>
-                  <h4>Frederick</h4>
-                  <h5>Marketing Officer</h5>
+                <div className="col-lg-10 m-auto">
+                  <div className="team-slider-text text-center">
+                    <i className="fas fa-quote-left"></i>
+                    <p>
+                      This is Photoshop's version of Lorem Ipsum. Proin gravida
+                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                      quis bibendum auctor, nisi elit consequat ipsum, nec
+                      sagittis sem nibh id elit. Duis sed odio sit amet nibh
+                      vulputate cursus a sit amet mauris. Morbi accumsan ipsum
+                      velit. Nam nec tellus a odio tincidunt auctor a ornare
+                      odio. Sed non mauris vitae erat consequat auctor eu in
+                      elit.
+                    </p>
+                    <h4>Frederick</h4>
+                    <h5>Marketing Officer</h5>
+                  </div>
                 </div>
-              </div>
-            </Slider>
+              </Slider>
+            </div>
+
+            {/* <i className="fas fa-chevron-left team-arrow-left"></i>
+          <i className="fas fa-chevron-right team-arrow-right"></i> */}
+
+            <div className="row slider-img">
+              <Slider
+                asNavFor={nav1}
+                ref={(slider2) => setNav2(slider2)}
+                slidesToShow={5}
+                swipeToSlide={true}
+                focusOnSelect={true}
+                centerMode={true}
+                centerPadding="0px"
+                prevArrow={<PrevArrow />}
+                nextArrow={<NextArrow />}
+              >
+                <div className="col-lg-2">
+                  <div className="team-slider-img">
+                    <img src="img/man1.jpg" alt="image not found" />
+                  </div>
+                </div>
+                <div className="col-lg-2">
+                  <div className="team-slider-img">
+                    <img src="img/man02.jpg" alt="image not found" />
+                  </div>
+                </div>
+                <div className="col-lg-2">
+                  <div className="team-slider-img">
+                    <img src="img/man3.jpg" alt="image not found" />
+                  </div>
+                </div>
+                <div className="col-lg-2">
+                  <div className="team-slider-img">
+                    <img src="img/man4.jpg" alt="image not found" />
+                  </div>
+                </div>
+                <div className="col-lg-2">
+                  <div className="team-slider-img">
+                    <img src="img/man5.jpg" alt="image not found" />
+                  </div>
+                </div>
+                <div className="col-lg-2">
+                  <div className="team-slider-img">
+                    <img src="img/man6.jpg" alt="image not found" />
+                  </div>
+                </div>
+              </Slider>
+            </div>
           </div>
-
-          <i className="fas fa-chevron-left team-arrow-left"></i>
-          <i className="fas fa-chevron-right team-arrow-right"></i>
-
-          <div className="row slider-img">
-            <div className="col-lg-2">
-              <div className="team-slider-img">
-                <img src="img/man1.jpg" alt="image not found" />
-              </div>
-            </div>
-            <div className="col-lg-2">
-              <div className="team-slider-img">
-                <img src="img/man02.jpg" alt="image not found" />
-              </div>
-            </div>
-            <div className="col-lg-2">
-              <div className="team-slider-img">
-                <img src="img/man3.jpg" alt="image not found" />
-              </div>
-            </div>
-            <div className="col-lg-2">
-              <div className="team-slider-img">
-                <img src="img/man4.jpg" alt="image not found" />
-              </div>
-            </div>
-            <div className="col-lg-2">
-              <div className="team-slider-img">
-                <img src="img/man5.jpg" alt="image not found" />
-              </div>
-            </div>
-            <div className="col-lg-2">
-              <div className="team-slider-img">
-                <img src="img/man6.jpg" alt="image not found" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </Fade>
     </>
   );
 };
